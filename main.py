b482d67ec7d543714caf7522414e076c9c65aacc
drago-suzuki58 <dorasuzu58@gmail.com>
2025-05-09 00:13:39 +0900

    feat: 土台構造

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from ogp-creator!")
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from contextlib import asynccontextmanager
 
+import ogp_creator.db as db
+import ogp_creator.routers as routers
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db.init_db()
+    yield
+    pass
+
+app = FastAPI(lifespan=lifespan)
+
+app.mount("/statics", StaticFiles(directory="statics"), name="statics")
+templates = Jinja2Templates(directory="templates")
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ ogp_creator/db.py
@@ -0,0 +1,18 @@
+from sqlmodel import SQLModel, create_engine, Session
+import os
+
+import ogp_creator.env as env
+
+engine = create_engine(env.DATABASE_URL, echo=True)
+
+def init_db():
+    if env.DATABASE_URL.startswith("sqlite:///"):
+        db_path = env.DATABASE_URL.replace("sqlite:///", "")
+        db_dir = os.path.dirname(db_path)
+        if not os.path.exists(db_dir):
+            os.makedirs(db_dir, exist_ok=True)
+
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    return Session(engine)
